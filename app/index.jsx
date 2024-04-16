669d2a302980181b2d6e3b426a598c794844c4b9
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2024-04-16 01:32:18 -0700

    native wind basic setup has been done

--- app/index.jsx
+++ app/index.jsx
@@ -20,15 +20,7 @@ const index = () => {
         odio. Corporis odio tempore quas sapiente praesentium, repellat, libero
         deleniti dolorem ipsum suscipit aperiam perspiciatis enim? Lorem ipsum
         dolor sit amet consectetur adipisicing elit. Accusantium quibusdam
-        tenetur autem quaerat nobis odio. Corporis odio tempore quas sapiente
-        praesentium, repellat, libero deleniti dolorem ipsum suscipit aperiam
-        perspiciatis enim?. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Commodi eius harum laborum nemo beatae fugit labore molestias
-        maiores odit atque? Ea illum eaque vero, molestias nesciunt iusto quod
-        accusamus repellendus. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Commodi eius harum laborum nemo beatae fugit labore
-        molestias maiores odit atque? Ea illum eaque vero, molestias nesciunt
-        iusto quod accusamus repellendus.
+        tenetur autem quaerat nobis odio. 
       </Text>
       <Link href="/profile">Profile</Link>
     </View>
